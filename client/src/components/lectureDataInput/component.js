a5e2694e82b7a71e42b38665a1f9378fdc0c3397
Yuriy <yuriyhurnyak@gmail.com>
2017-03-26 18:49:40 +0300

    added lecture data input component

---
+++ client/src/components/lectureDataInput/component.js
@@ -0,0 +1,11 @@
+const LectureRowItemController = require ('./controller.js');
+
+const LectureRowItemComponent = {
+    bindings: {
+        lecture: '='
+    },
+    controller: LectureRowItemController,
+    template: require ('./template.html')
+}
+
+module.exports = LectureRowItemComponent;
