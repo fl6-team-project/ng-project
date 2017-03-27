379e8422baecc3786d9933f96f4867481986a073
Yuriy <yuriyhurnyak@gmail.com>
2017-03-27 19:26:13 +0300

    fixed label covering

--- client/src/components/lectureDataInput/controller.js
+++ client/src/components/lectureDataInput/controller.js
@@ -1,17 +1,28 @@
 require ('./style.scss');
 
-function LectureDataInputController($http) {
+function LectureDataInputController($http, $state, $timeout) {
     self = this;
 
+    //wait when rendering finished and update labels
+    $timeout(function () {
+
+        // @todo needed to fix the date picker
+        $('.datepicker').pickadate({
+            selectMonths: true, // Creates a dropdown to control month
+            selectYears: 3 // Creates a dropdown of 15 years to control year
+        });
+        Materialize.updateTextFields();
+    });
+
+
     self.saveEditing = function (id, object) {
-        console.log(object);
         $http.put('/api/lectures/'+id, object).then(function(res) {
             self.lectures = res.data;
+            $state.go('teacher.lectures');
         });
     };
 
-
 }
 
-LectureDataInputController.$inject = ['$http'];
+LectureDataInputController.$inject = ['$http', "$state", "$timeout"];
 module.exports = LectureDataInputController;
